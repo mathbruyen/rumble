7597ef196d5c606ce9294eb584b34d3420419504
Mathieu Bruyen <code@mais-h.eu>
2012-03-31 19:16:31 +0200

    Directly define the workflow in the main script file

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+var app = require('express').createServer();
+var io = require('socket.io').listen(app);
+
 /**
  * ---CONFIGURATION
  */
+
 var config = {
   grid: {
     size: 100
@@ -14,55 +18,8 @@ var config = {
 }
 
 /**
- * ---WORKFLOW
- */
-var workflow = new require('./workflow')();
-
-workflow.players = {};
-workflow.grid = new Array(config.grid.size);
-
-var playersForUI = function() {
-  var players = new Array();
-  for (var name in workflow.players) {
-    players.push({
-      name: name,
-      score: workflow.players[name].score
-    });
-  }
-  return players;
-}
-
-workflow.setNode('init', 'chooseusername', function(data) {
-  if ((!data.name) || (data.name == 'N/A') || (data.name.length > 20)) {
-    this.socket.emit('wrongusername', { reason: 'Username is incorrect' });
-  } else if (workflow.players[data.name]) {
-    this.socket.emit('wrongusername', { reason: 'Username already in use' });
-  } else {
-    this.workflow.players[data.name] = {
-      score: 0
-    };
-    this.player = this.workflow.players[data.name];
-    this.username = data.name;
-    this.socket.emit('entergame', {
-      players: playersForUI(),
-      gridsize: config.grid.size
-    });
-    return 'choosenumber';
-  }
-});
-workflow.setNode('*', 'disconnect', function(data, local, global) {
-  if (this.username) {
-    delete this.workflow.players[this.username];
-  }
-  delete this.socket;
-  //TODO clean grid entry
-  //TODO notify players
-});
-
-/**
  * ---EXPRESS
  */
-var app = require('express').createServer()
 
 app.listen(config.express.port);
 app.set('view engine', 'jade');
@@ -78,14 +35,72 @@ app.get('/rumble.js', function(req, res) {
 });
 
 /**
+ * ---PLAYERMANAGEMENT
+ */
+
+var serverState = {
+  players: {},
+  grid: new Array(config.grid.size)
+}
+
+var playersForUI = function() {
+  var players = new Array();
+  for (var name in serverState.players) {
+    players.push({
+      name: name,
+      score: serverState.players[name].score
+    });
+  }
+  return players;
+}
+
+/**
  * ---WEBSOCKETS
  */
-var io = require('socket.io').listen(app);
+
+var bindEvent = function(socket, event, state, callback) {
+  var setState = function(newState) {
+    if (newState) {
+      socket.state = newState;
+    }
+  }
+  socket.on(event, function(data) {
+    if ((state == '*') || (socket.state == state)) {
+      // TODO Pass setState as an additional argument so that the state could
+      // be changed both synchronously and asynchronously but then concurrent
+      // actions on the workflow could be tricky or need something like
+      // Connect's next method
+      setState(callback.call(socket, data));
+    }
+  });
+}
 
 io.sockets.on('connection', function(socket) {
-  var f = workflow.instance('init');
-  f.socket = socket;
-  f.listen(socket);
+  socket.state = 'init';
+  bindEvent(socket, 'disconnect', '*', function(data) {
+    console.log('disconnect');
+    if (this.player) {
+      delete serverState.players[this.player.username];
+    }
+  });
+  bindEvent(socket, 'chooseusername', 'init', function(data) {
+    if ((!data.name) || (data.name == 'N/A') || (data.name.length > 20)) {
+      this.emit('wrongusername', { reason: 'Username is incorrect' });
+    } else if (serverState.players[data.name]) {
+      this.emit('wrongusername', { reason: 'Username already in use' });
+    } else {
+      serverState.players[data.name] = {
+        username: data.name,
+        score: 0
+      };
+      this.player = serverState.players[data.name];
+      this.emit('entergame', {
+        players: playersForUI(),
+        gridsize: config.grid.size
+      });
+      return 'choosenumber';
+    }
+  });
 });
 
 /**
